c1a687688ed8346ae571a274bb0bd34d619f3d8d
Johan de Groot <johan.groot@webking.se>
2021-12-26 19:37:06 +0100

    gui setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,13 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-   <div className="h-screen w-screen flex flex-col">
-     <div className=""></div>
-     <div className=""></div>
+   <div className="h-screen w-screen items-center justify-center flex flex-col">
+     <div className="text-2xl text-center">Which Pokémon is Rounder?</div>
+     <div className="border mt-3 items-center rounded p-8 flex justify-between max-w-2xl">
+       <div className="w-16 h-16 bg-red-200"></div>
+       <div className="px-8">Vs</div>
+       <div className="w-16 h-16 bg-red-200"></div>
+     </div>
    </div>
   )
 }
